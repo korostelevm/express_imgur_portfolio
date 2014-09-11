e9a9a215316528b1ed7511719af99e9719b7da48
Michael Korostelev <macbook@Michaels-MacBook-Pro.local>
2014-09-10 23:38:29 -0400

    initial commit

---
+++ routes/index.js
@@ -0,0 +1,38 @@
+var express = require('express');
+var router = express.Router();
+
+var request = require('request');
+var cheerio = require('cheerio');
+
+
+/* GET home page. */
+router.get('/', function(req, res) {
+    
+ var albums = [];   
+request('http://korostelevm.imgur.com', function (error, response, body) {
+  if (!error && response.statusCode == 200) {
+    //console.log(body) // Print the google web page.
+    $ = cheerio.load(body);
+   $('#items').children('.album').each(function(i, elem) {
+      albums[i] = {albumTitle: $(this).attr('data-title'),
+                  albumCover: "imgur.com/"+$(this).attr('data-cover'),
+                  albumDescription: $(this).attr('data-description'),
+                  albumMetaData: $(this).children('.metadata').text().split('\n                                ')};
+          
+          
+      
+       //console.log($(this).attr('data-title'));
+    });
+      
+      
+      	console.log(albums);
+  
+    
+    res.render('index', { title: "imgur test", albums: albums });
+  }
+})
+
+    
+});
+
+module.exports = router;
